6f258186944df732397c37552f907b72419232b4
fabianrguez <fabian.rguezg@gmail.com>
2022-01-20 14:51:52 +0000

    load more packages when scroll to last element

--- src/js/skypack.js
+++ src/js/skypack.js
@@ -4,13 +4,18 @@ const searchPackageElement = document.querySelector('input[name="search-packages
 const packageResultList = document.querySelector('.package__list');
 let previousSearchResult = [];
 let actualPage = 1;
+let totalPages = 0;
 
 const API_URL = 'https://api.skypack.dev/v1';
 const CDN_URL = 'https://cdn.skypack.dev';
 
 async function fetchPackages({ packageName, page = 1 }) {
   const response = await fetch(`${API_URL}/search?q=${packageName}&p=${page}`);
-  return await response.json();
+  const data = await response.json();
+
+  actualPage = data.meta.page;
+
+  return data;
 }
 
 function getBadge({ popularityScore }) {
@@ -53,18 +58,42 @@ function renderResultPackages({ results }) {
 
     packageResultList.appendChild(liElement);
   });
+
+  createScrollEndObserver(packageResultList.lastElementChild);
 }
 
-function fetchMorePackages({ packageName, page }) {
-  const { results } = fetchPackages({ packageName, page });
-  previousSearchResult = [...previousSearchResult, results];
-  renderResultPackages({ previousSearchResult });
+function createScrollEndObserver(element) {
+  const observer = new IntersectionObserver(
+    async ([entry], observer) => {
+      if (entry.isIntersecting) {
+        observer.disconnect();
+        if (actualPage + 1 <= totalPages) {
+          await fetchMorePackages({ packageName: searchPackageElement.value, page: actualPage + 1 });
+        }
+      }
+    },
+    {
+      root: null,
+      rootMargin: '0px',
+      threshold: '1.0',
+    }
+  );
+  observer.observe(element);
+}
+
+async function fetchMorePackages({ packageName, page }) {
+  const { results } = await fetchPackages({ packageName, page });
+  previousSearchResult = [...previousSearchResult, ...results];
+  renderResultPackages({ results: previousSearchResult });
 }
 
 async function handleSearch() {
   if (this.value) {
-    const { results } = await fetchPackages({ packageName: this.value });
+    previousSearchResult = [];
+    const { results, meta } = await fetchPackages({ packageName: this.value });
+    ({ totalPages } = meta);
     results && renderResultPackages({ results });
+    previousSearchResult = results;
     return;
   }
   packageResultList.setAttribute('hidden', '');
